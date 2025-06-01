08e84dac8ad05f8d4f58798e937b2146a00dc380
vudat1998 <44422264+vudat1998@users.noreply.github.com>
2025-06-01 10:20:15 +0700

    Update rotate_cloud247_.sh

--- rotate_cloud247_.sh
+++ rotate_cloud247_.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-WORKDIR="/home/proxy-installer"
-WORKDATA="${WORKDIR}/data.txt"
 
-clear_proxy_and_file() {
-    > /usr/local/etc/3proxy/3proxy.cfg
-    > $WORKDIR/data.txt
-    > $WORKDIR/proxy.txt
-    chmod +x ${WORKDIR}/boot_ifconfig_delete.sh ${WORKDIR}/boot_iptables_delete.sh
-    bash ${WORKDIR}/boot_ifconfig_delete.sh
-    bash ${WORKDIR}/boot_iptables_delete.sh
-    pkill -f 3proxy
-    systemctl restart NetworkManager
-    > ${WORKDIR}/boot_iptables.sh
-    > ${WORKDIR}/boot_ifconfig.sh
+check_iptables_install() {
+    if ! iptables -V &> /dev/null
+    then
+        echo "iptables chưa được cài đặt. Đang tiến hành cài đặt..."
+        yum install -y iptables-services
+        systemctl enable iptables
+        systemctl start iptables
+    else
+        echo "iptables đã được cài đặt."
+    fi
 }
 
 random() {
@@ -26,6 +23,30 @@ gen64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
     }
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+
+install_3proxy() {
+    if [ -f /usr/local/etc/3proxy/bin/3proxy ]; then
+        echo "3proxy đã được cài đặt. Bỏ qua bước cài đặt."
+        return
+    fi
+    echo "Đang cài đặt 3proxy..."
+    URL="https://raw.githubusercontent.com/vudat1998/Vinmmoproxy/main/3proxy-3proxy-0.9.4.tar.gz"Add commentMore actions
+    yum install -y gcc bsdtar make net-tools zip >/dev/null
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-0.9.4 || exit
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp bin/3proxy /usr/local/etc/3proxy/bin/
+    cp scripts/3proxy.service /etc/systemd/system/3proxy.service
+    sed -i 's|Environment=CONFIGFILE=/etc/3proxy/3proxy.cfg|Environment=CONFIGFILE=/usr/local/etc/3proxy/3proxy.cfg|' /etc/systemd/system/3proxy.service
+    sed -i 's|ExecStart=/bin/3proxy ${CONFIGFILE}|ExecStart=/usr/local/etc/3proxy/bin/3proxy ${CONFIGFILE}|' /etc/systemd/system/3proxy.service
+    sed -i 's/RestartSec=60s/RestartSec=0s/' /etc/systemd/system/3proxy.service
+    chmod +x /usr/local/etc/3proxy/bin/3proxy
+    systemctl daemon-reload
+    systemctl enable 3proxy
+    cd ..
+    rm -rf 3proxy-0.9.4
 }
 
 gen_3proxy() {
@@ -47,8 +68,8 @@ EOF
 }
 
 gen_proxy_file_for_user() {
-    cat > $WORKDIR/proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2}' ${WORKDATA})
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
@@ -74,7 +95,15 @@ gen_ifconfig_delete() {
     awk -F "/" '{print "ifconfig eth0 inet6 del " $5 "/64"}' ${WORKDATA}
 }
 
-clear_proxy_and_file
+echo "Bắt đầu cài đặt các ứng dụng cần thiết..."
+yum install -y gcc net-tools bsdtar zip curl >/dev/null
+check_iptables_install
+install_3proxy
+
+echo "Thiết lập thư mục làm việc /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir -p $WORKDIR && cd $WORKDIR || exit
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
@@ -87,26 +116,27 @@ read -r COUNT
 FIRST_PORT=10000
 LAST_PORT=$((FIRST_PORT + COUNT - 1))
 
-gen_data > $WORKDIR/data.txt
+gen_data > $WORKDATA
 
-gen_iptables > $WORKDIR/boot_iptables.sh
 gen_iptables_delete > $WORKDIR/boot_iptables_delete.sh
+gen_iptables > $WORKDIR/boot_iptables.sh
 
-gen_ifconfig > $WORKDIR/boot_ifconfig.sh
 gen_ifconfig_delete > $WORKDIR/boot_ifconfig_delete.sh
+gen_ifconfig > $WORKDIR/boot_ifconfig.sh
 
-chmod +x ${WORKDIR}/boot_*.sh
+chmod +x $WORKDIR/boot_*.sh
 
 gen_3proxy > /usr/local/etc/3proxy/3proxy.cfg
-
-ulimit -n 10048
+chmod +x /usr/local/etc/3proxy/3proxy.cfg
 
 bash $WORKDIR/boot_iptables.sh
 bash $WORKDIR/boot_ifconfig.sh
+
+ulimit -n 10048
 
 gen_proxy_file_for_user
 
 systemctl daemon-reload
 systemctl restart 3proxy
 
-echo "Rotate Done"
+echo "Install Done"
